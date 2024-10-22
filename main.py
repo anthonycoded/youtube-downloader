6f99befb2378f35a0820d9e7fd4986fecd403994
Shawn <barbels33@gmail.com>
2024-10-22 13:07:25 -0400

    added gui with ui messaging

--- main.py
+++ main.py
@@ -1,47 +1,66 @@
+from tkinter import PhotoImage
+
 from pytubefix import YouTube
-import tkinter as tk
-from tkinter import filedialog
+from customtkinter import *
+from PIL import Image
 import ffmpeg
 import os
 
 save_path = "C:/Users/barbe/videos"
 #test_url = "https://www.youtube.com/watch?v=VTB0_SBltDw"
 
+app = CTk()
+app.title("Youtube Downloader")
+app.geometry("500x400")
+#app.config(width=500, padx=30, pady=30)
+
+img = CTkImage(Image.open("image.png"))
+
+status = "Ready"
+
+
 def download_video():
     try:
-        video_url = input("Please enter a Youtube url: ")
-        yt = YouTube(video_url)
-        video_stream = yt.streams.filter(progressive=False, file_extension="webm", adaptive=True, only_video=True).get_highest_resolution(progressive=False)
-        audio_stream = yt.streams.filter(progressive=False, file_extension="webm", adaptive=True, only_audio=True)[0]
+        global status
+        video_url = youtube_url.get()
 
-        print("Downloading video...")
-        video_file= video_stream.download(output_path=save_path, filename=f"video-{yt.title}webm", )
+        if len(video_url) > 0:
+            status_label.configure(text="Initializing Download", text_color="#0ce889", height=22)
+            button.destroy()
+            url_entry.delete(0, "end")
+            app.update()
 
-        print(f"Downloading audio...")
-        audio_file = audio_stream.download(output_path=save_path, filename=f"audio-{yt.title}webm")
-#        print(audio_file)
-        merge_audio_video(title=yt.title, audio_file=audio_file, video_file=video_file)
-        #
-        #
-        # highest_res_stream.download(output_path=save_path)
+            yt = YouTube(video_url)
+            video_stream = yt.streams.filter(progressive=False, file_extension="webm", adaptive=True,
+                                             only_video=True).get_highest_resolution(progressive=False)
+            audio_stream = yt.streams.filter(progressive=False, file_extension="webm", adaptive=True, only_audio=True)[
+                0]
 
+            status_label.configure(text="Downloading video...", text_color="#0ce889", height=22)
+            app.update()
+            video_file = video_stream.download(output_path=save_path, filename=f"video-{yt.title}webm", )
 
+            status_label.configure(text="Downloading audio...", text_color="#0ce889", height=22)
+            app.update()
+            audio_file = audio_stream.download(output_path=save_path, filename=f"audio-{yt.title}webm")
+            merge_audio_video(title=yt.title, audio_file=audio_file, video_file=video_file)
+        else:
+            status_label.configure(text="Please enter a valid url.", text_color="#ed0909", height=22)
+            app.update()
     except Exception as e:
-        print(e)
 
+        status_label.configure(text=f"Error: {e}", text_color="#ed0909", height=22)
+        app.update()
 
-def open_file_dialog():
-    folder = filedialog.askdirectory()
-    if folder:
-        print(f"Selected Folder: {folder}")
-    return folder
 
 
 def merge_audio_video(audio_file, video_file, title):
-    print("Processing downloaded media...")
+    global button
+    status_label.configure(text="Reformatting downloaded media...", text_color="#0ce889", height=22)
+    app.update()
     try:
-        video = ffmpeg.input(filename=video_file,).video
-        audio = ffmpeg.input( filename=audio_file,).audio
+        video = ffmpeg.input(filename=video_file, ).video
+        audio = ffmpeg.input(filename=audio_file, ).audio
 
         result = ffmpeg.output(
             audio,
@@ -52,23 +71,45 @@ def merge_audio_video(audio_file, video_file, title):
             format="mp4"
         ).run()
 
-        print ("Finishing up...")
+        print("Finishing up...")
 
         if result:
-           os.remove(audio_file)
-           os.remove(video_file)
-           print(f"Video downloaded successfully {title}")
+            os.remove(audio_file)
+            os.remove(video_file)
+            status_label.configure(text=f"Video downloaded successfully {title}", text_color="#0ce889", height=22, width=300)
+            button = CTkButton(app, text="Download Video", command=download_video, corner_radius=25, image=img)
+            button.place(relx=0.5, rely=0.5, anchor="center")
+            app.update()
+
+
         else:
-            print("Something went wrong")
+
+            status_label.configure(text="Something went wrong", text_color="#0ce889", height=22)
+            app.update()
+
 
     except Exception as e:
-        print(e)
+        status_label.configure(text=f"Error: {e}", text_color="#0ce889", height=22)
+        app.update()
 
+
+youtube_url = StringVar()
+# logo = CTkImage()
+# logo.place(relx=0.5, rely=0.1, anchor="center")
+# canvas.create_image( image=img)
+
+url_label = CTkLabel(app, text="Please enter a youtube video url.")
+url_label.place(relx=0.5, rely=0.1, anchor="center")
+
+url_entry = CTkEntry(app, width=400, textvariable=youtube_url,)
+url_entry.place(relx=0.5, rely=0.2, anchor="center")
+url_entry.focus()
+
+status_label = CTkLabel(app, text=status)
+status_label.place(relx=0.5, rely=0.3, anchor="center")
+
+button = CTkButton(app, text="Download Video", command=download_video, corner_radius=25, image=img)
+button.place(relx=0.5, rely=0.5, anchor="center")#grid(row=7, column=2, padx=20, pady=10, )
 
 if __name__ == "__main__":
-    root = tk.Tk()
-    root.withdraw()
-
-
-
-    download_video()
+    app.mainloop()
